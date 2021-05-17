40eed40caec5a759706598d5c54d8da76c4cf176
Agon Karimani <agonkarimani@gmail.com>
2021-05-17 02:38:41 +0200

    Add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import React, { useState, useEffect } from "react";
-import "App.css";
-import Cards from "Cards.jsx";
+import "./App.css";
+import "./Cards.jsx";
+import Cards from "./Cards.jsx";
 
 export default function App() {
-  const [characters, setCharacters] = useState([]);
   const url = "https://rickandmortyapi.com/api/character/?page=1";
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
-      .then((resBody) => setCharacters(resBody.results))
-      .catch((error) => console.error(error));
-  }, []);
+      .then((resBody) => setCharacters(resBody.results));
+  }, [url]);
 
   return (
     <div className="App">
+      <h1 className="App">Rick & Morty Characters</h1>
       {characters.map((character) => {
-        const { id, name, gender, species, status } = character;
+        const { name, gender, species, status } = character;
+
         return (
           <Cards
-            key={id}
             name={name}
             gender={gender}
             species={species}
@@ -27,6 +28,7 @@ export default function App() {
           />
         );
       })}
+      <h1 className="App">Rick & Morty Characters</h1>
     </div>
   );
 }
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,22 +1,22 @@
-import { useState } from "react";
-import "./Card.css";
+import React, { useState } from "react";
+import "./Cards.css";
 
-export default function Card({ name, gender, species, status }) {
+export default function Cards({ name, gender, species, status }) {
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <section className="Card">
-      <h2>{name}</h2>
-      <button onClick={() => setIsActive(!isActive)}>Show</button>
-      {isActive && (
-        <>
-          <ul>
+    <section className="Cards">
+      <div className="Cards_style">
+        <h2 className="Cards__header">{name}</h2>
+        <button onClick={() => setIsActive(!isActive)}>Show more</button>
+        {isActive && (
+          <ul className="Cards__list">
             <li>{gender}</li>
             <li>{species}</li>
             <li>{status}</li>
           </ul>
-        </>
-      )}
+        )}
+      </div>
     </section>
   );
 }
